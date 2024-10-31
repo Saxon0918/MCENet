0e8964a23854a3c693d012b49b16e65915a6d655
zhuhaoran <943764981@qq.com>
2024-10-31 16:22:13 +0800

    baseline change

--- baselines/mlp.py
+++ baselines/mlp.py
@@ -1,145 +1,107 @@
-from src.preprocess_data import ADNI_Dataset, ADNI_ALL_Dataset
+from src.preprocess_data import ADNI_Dataset, ADNI_three_Dataset
 import torch
 from torch.utils.data import DataLoader
 import torch.nn as nn
 import torch.optim as optim
 from sklearn.metrics import accuracy_score, roc_auc_score, confusion_matrix
+from src.eval_metrics import calculate_metric, compute_metrics
 
-train_data = ADNI_Dataset(random_seed=1111, mode='train')
-valid_data = ADNI_Dataset(random_seed=1111, mode='val')
-test_data = ADNI_Dataset(random_seed=1111, mode='test')
-
-if torch.cuda.is_available():
-    g_cuda = torch.Generator(device='cuda')
-else:
-    g_cuda = torch.Generator()
-
-g_cpu = torch.Generator(device='cpu')
-
-train_loader = DataLoader(train_data, batch_size=8, shuffle=True, generator=g_cpu)
-valid_loader = DataLoader(valid_data, batch_size=8, shuffle=True, generator=g_cpu)
-test_loader = DataLoader(test_data, batch_size=8, shuffle=True, generator=g_cpu)
-
-device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
 class MLP(nn.Module):
     def __init__(self):
         super(MLP, self).__init__()
         self.fc1 = nn.Linear(140 + 140 + 140 + 100, 256)
         self.fc2 = nn.Linear(256, 128)
-        self.fc3 = nn.Linear(128, 2)
+        self.fc3 = nn.Linear(128, 3)
 
-    def forward(self, x1, x2, x3, x4):
-        x = torch.cat([x1, x2, x3, x4], dim=1)  # 拼接输入
+    def forward(self, mri, av45, gene, fdg):
+        x = torch.cat([mri, av45, gene, fdg], dim=1)  # 拼接输入
         x = torch.relu(self.fc1(x))
         x = torch.relu(self.fc2(x))
         x = self.fc3(x)
         return x
 
-model = MLP().to(device)
-criterion = nn.CrossEntropyLoss()
-optimizer = optim.Adam(model.parameters(), lr=0.001)
-
-def calc_sen_spe(y_true, y_pred):
-    cm = confusion_matrix(y_true, y_pred)
-    TN, FP, FN, TP = cm.ravel()
-    sen = TP / (TP + FN)
-    spe = TN / (TN + FP)
-    return sen, spe
 
 def train_and_evaluate(train_loader, valid_loader, model, criterion, optimizer, epochs=10):
     for epoch in range(epochs):
         model.train()
         train_loss = 0
-        all_preds = []
-        all_labels = []
 
         for i_batch, data in enumerate(train_loader):
-            x1 = data['x1']
-            x2 = data['x2']
-            x4 = data['x4']
-            x3 = data['x3']
-            labels = data['label']
-
-            x1, x2, x3, x4, labels = x1.to(device), x2.to(device), x3.to(device), x4.to(device), labels.to(device)
+            mri = data['mri']
+            av45 = data['av45']
+            fdg = data['fdg']
+            gene = data['gene']
+            label = data['label']
+            mri, av45, fdg, gene, label = mri.to(device), av45.to(device), fdg.to(device), gene.to(device), label.to(
+                device)
 
             optimizer.zero_grad()
-            outputs = model(x1, x2, x3, x4)
-            loss = criterion(outputs, labels)
+            outputs = model(mri, av45, fdg, gene)
+            loss = criterion(outputs, label)
             loss.backward()
             optimizer.step()
-
             train_loss += loss.item()
-
-            _, preds = torch.max(outputs, 1)
-            all_preds.append(preds)
-            all_labels.append(labels)
-
-        all_preds = torch.cat(all_preds).cpu().numpy()  # 转移到CPU计算
-        all_labels = torch.cat(all_labels)
-        all_labels = torch.argmax(all_labels, dim=1).cpu().numpy()  # 转换为类别索引
-        acc = accuracy_score(all_labels, all_preds)
-        auc = roc_auc_score(all_labels, all_preds)
-        sen, spe = calc_sen_spe(all_labels, all_preds)
-
-        print(f'Epoch {epoch+1}/{epochs} - Loss: {train_loss:.4f}, ACC: {acc:.4f}, AUC: {auc:.4f}, SEN: {sen:.4f}, SPE: {spe:.4f}')
 
         # 验证模型
         model.eval()
-        valid_preds = []
-        valid_labels = []
         with torch.no_grad():
             for i_batch, data in enumerate(valid_loader):
-                x1 = data['x1']
-                x2 = data['x2']
-                x4 = data['x4']
-                x3 = data['x3']
-                labels = data['label']
+                mri = data['mri']
+                av45 = data['av45']
+                fdg = data['fdg']
+                gene = data['gene']
+                label = data['label']
 
-                x1, x2, x3, x4, labels = x1.to(device), x2.to(device), x3.to(device), x4.to(device), labels.to(device)
-                outputs = model(x1, x2, x3, x4)
-                _, preds = torch.max(outputs, 1)
-                valid_preds.append(preds)
-                valid_labels.append(labels)
-
-        valid_preds = torch.cat(valid_preds).cpu().numpy()
-        valid_labels = torch.cat(valid_labels)
-        valid_labels = torch.argmax(valid_labels, dim=1).cpu().numpy()  # 转换为类别索引
-        acc = accuracy_score(valid_labels, valid_preds)
-        auc = roc_auc_score(valid_labels, valid_preds)
-        sen, spe = calc_sen_spe(valid_labels, valid_preds)
-
-        print(f'Validation - ACC: {acc:.4f}, AUC: {auc:.4f}, SEN: {sen:.4f}, SPE: {spe:.4f}')
-
-
-train_and_evaluate(train_loader, valid_loader, model, criterion, optimizer, epochs=20)
+                mri, av45, fdg, gene, label = mri.to(device), av45.to(device), fdg.to(device), gene.to(
+                    device), label.to(device)
+                outputs = model(mri, av45, fdg, gene)
+                compute_metrics(outputs, label)
 
 
 def evaluate_test(test_loader, model):
     model.eval()
-    test_preds = []
-    test_labels = []
     with torch.no_grad():
         for i_batch, data in enumerate(test_loader):
-            x1 = data['x1']
-            x2 = data['x2']
-            x4 = data['x4']
-            x3 = data['x3']
-            labels = data['label']
+            mri = data['mri']
+            av45 = data['av45']
+            fdg = data['fdg']
+            gene = data['gene']
+            label = data['label']
 
-            x1, x2, x3, x4, labels = x1.to(device), x2.to(device), x3.to(device), x4.to(device), labels.to(device)
-            outputs = model(x1, x2, x3, x4)
-            _, preds = torch.max(outputs, 1)
-            test_preds.append(preds)
-            test_labels.append(labels)
+            mri, av45, fdg, gene, label = mri.to(device), av45.to(device), fdg.to(device), gene.to(device), label.to(
+                device)
+            outputs = model(mri, av45, fdg, gene)
+            compute_metrics(outputs, label)
 
-    test_preds = torch.cat(test_preds).cpu().numpy()
-    test_labels = torch.cat(test_labels)
-    test_labels = torch.argmax(test_labels, dim=1).cpu().numpy()  # 转换为类别索引
-    acc = accuracy_score(test_labels, test_preds)
-    auc = roc_auc_score(test_labels, test_preds)
-    sen, spe = calc_sen_spe(test_labels, test_preds)
 
-    print(f'Test Set - ACC: {acc:.5f}, AUC: {auc:.5f}, SEN: {sen:.5f}, SPE: {spe:.5f}')
+if __name__ == '__main__':
 
-evaluate_test(test_loader, model)
+    # train_data = ADNI_Dataset(random_seed=1, mode='train')
+    # valid_data = ADNI_Dataset(random_seed=1, mode='val')
+    # test_data = ADNI_Dataset(random_seed=1, mode='test')
+
+    # three class
+    train_data = ADNI_three_Dataset(random_seed=2, mode='train')
+    valid_data = ADNI_three_Dataset(random_seed=2, mode='val')
+    test_data = ADNI_three_Dataset(random_seed=2, mode='test')
+
+    if torch.cuda.is_available():
+        g_cuda = torch.Generator(device='cuda')
+    else:
+        g_cuda = torch.Generator()
+
+    g_cpu = torch.Generator(device='cpu')
+
+    train_loader = DataLoader(train_data, batch_size=128, shuffle=True, generator=g_cpu)
+    valid_loader = DataLoader(valid_data, batch_size=128, shuffle=True, generator=g_cpu)
+    test_loader = DataLoader(test_data, batch_size=128, shuffle=True, generator=g_cpu)
+
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+    model = MLP().to(device)
+    criterion = nn.CrossEntropyLoss()
+    optimizer = optim.Adam(model.parameters(), lr=0.001)
+
+    train_and_evaluate(train_loader, valid_loader, model, criterion, optimizer, epochs=20)
+    print("this is test result")
+    evaluate_test(test_loader, model)
--- baselines/svm.py
+++ baselines/svm.py
@@ -1,4 +1,4 @@
-from src.preprocess_data import ADNI_Dataset, ADNI_ALL_Dataset
+from src.preprocess_data import ADNI_Dataset, ADNI_three_Dataset
 import torch
 from torch.utils.data import DataLoader
 import torch.nn as nn
@@ -6,80 +6,102 @@ import torch.optim as optim
 import numpy as np
 from sklearn.svm import SVC
 from sklearn.metrics import accuracy_score, roc_auc_score, confusion_matrix
+from sklearn.preprocessing import label_binarize
 
-train_data = ADNI_Dataset(random_seed=1111, mode='train')
-valid_data = ADNI_Dataset(random_seed=1111, mode='val')
-test_data = ADNI_Dataset(random_seed=1111, mode='test')
 
-# 三分类
-# train_data = ADNI_ALL_Dataset(random_seed=args.seed, mode='train')
-# valid_data = ADNI_ALL_Dataset(random_seed=args.seed, mode='val')
-# test_data = ADNI_ALL_Dataset(random_seed=args.seed, mode='test')
-
-if torch.cuda.is_available():
-    g_cuda = torch.Generator(device='cuda')
-else:
-    g_cuda = torch.Generator()
-
-g_cpu = torch.Generator(device='cpu')
-
-train_loader = DataLoader(train_data, batch_size=8, shuffle=True, generator=g_cpu)
-valid_loader = DataLoader(valid_data, batch_size=8, shuffle=True, generator=g_cpu)
-test_loader = DataLoader(test_data, batch_size=8, shuffle=True, generator=g_cpu)
-
-device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-
-# 将数据转换为 NumPy 格式
-def loader_to_numpy(train_loader):
+def loader_to_numpy(data_loader):
     X = []
     Y = []
-    for i_batch, data in enumerate(train_loader):
-        x1 = data['x1']
-        x2 = data['x2']
-        x4 = data['x4']
-        x3 = data['x3']
-        labels = data['label']
-        # 将数据从 GPU 转移到 CPU 并转换为 NumPy
-        x1, x2, x3, x4 = x1.cpu().numpy(), x2.cpu().numpy(), x3.cpu().numpy(), x4.cpu().numpy()
-        labels = torch.argmax(labels, dim=1).cpu().numpy()  # 将one-hot标签转换为类别
-        # 拼接输入 x1, x2, x3, x4
-        X.append(np.concatenate([x1, x2, x3, x4], axis=1))
-        Y.append(labels)
-    # 将列表转换为 NumPy 数组
+    for i_batch, data in enumerate(data_loader):
+        mri = data['mri']
+        av45 = data['av45']
+        fdg = data['fdg']
+        gene = data['gene']
+        label = data['label']
+        mri, av45, fdg, gene = mri.cpu().numpy(), av45.cpu().numpy(), fdg.cpu().numpy(), gene.cpu().numpy()
+        label = torch.argmax(label, dim=1).cpu().numpy()
+        X.append(np.concatenate([mri, av45, fdg, gene], axis=1))
+        Y.append(label)
     return np.vstack(X), np.hstack(Y)
 
-# 加载训练和验证数据
-X_train, Y_train = loader_to_numpy(train_loader)
-X_valid, Y_valid = loader_to_numpy(valid_loader)
 
-# 初始化 SVM 模型
-svm_model = SVC(kernel='linear', probability=True)  # 使用线性核函数，并启用概率估计
-
-# 训练 SVM 模型
-svm_model.fit(X_train, Y_train)
-
-# 验证 SVM 模型
 def evaluate_svm(X, Y, model):
     preds = model.predict(X)
-    probas = model.predict_proba(X)[:, 1]  # 提取属于类别1的概率
+    probas = model.predict_proba(X)[:, 1]
     acc = accuracy_score(Y, preds)
     auc = roc_auc_score(Y, probas)
     cm = confusion_matrix(Y, preds)
     TN, FP, FN, TP = cm.ravel()
-    sen = TP / (TP + FN)  # 敏感度
-    spe = TN / (TN + FP)  # 特异度
+    sen = TP / (TP + FN)
+    spe = TN / (TN + FP)
     print(f'ACC: {acc:.5f}, AUC: {auc:.5f}, SEN: {sen:.5f}, SPE: {spe:.5f}')
 
-# 评估训练集
-print("Training Set Evaluation:")
-evaluate_svm(X_train, Y_train, svm_model)
 
-# 评估验证集
-print("Validation Set Evaluation:")
-evaluate_svm(X_valid, Y_valid, svm_model)
+def evaluate_three_svm(X, Y, model):
+    Y_pred = model.predict(X)
+    Y_prob = model.predict_proba(X)
+    accuracy = accuracy_score(Y, Y_pred)
+    Y_binarized = label_binarize(Y, classes=np.unique(Y))
+    auc = roc_auc_score(Y_binarized, Y_prob, average="macro", multi_class="ovr")
 
-# 评估测试集
-X_test, Y_test = loader_to_numpy(test_loader)
-print("Test Set Evaluation:")
-evaluate_svm(X_test, Y_test, svm_model)
+    cm = confusion_matrix(Y, Y_pred)
+    specificity, sensitivity = [], []
+    for i in range(len(cm)):
+        tn = np.sum(cm) - (np.sum(cm[i, :]) + np.sum(cm[:, i]) - cm[i, i])
+        fp = np.sum(cm[:, i]) - cm[i, i]
+        fn = np.sum(cm[i, :]) - cm[i, i]
+        tp = cm[i, i]  # 真阳性
 
+        spe = tn / (tn + fp) if (tn + fp) != 0 else 0
+        sen = tp / (tp + fn) if (tp + fn) != 0 else 0
+
+        specificity.append(spe)
+        sensitivity.append(sen)
+
+    avg_specificity = np.mean(specificity)
+    avg_sensitivity = np.mean(sensitivity)
+    print(f"Accuracy: {accuracy}")
+    print(f"AUC: {auc}")
+    print(f"Sensitivity (Recall): {avg_sensitivity}")
+    print(f"Specificity: {avg_specificity}")
+
+
+if __name__ == '__main__':
+    # train_data = ADNI_Dataset(random_seed=1111, mode='train')
+    # valid_data = ADNI_Dataset(random_seed=1111, mode='val')
+    # test_data = ADNI_Dataset(random_seed=1111, mode='test')
+
+    # three class
+    train_data = ADNI_three_Dataset(random_seed=11, mode='train')
+    valid_data = ADNI_three_Dataset(random_seed=11, mode='val')
+    test_data = ADNI_three_Dataset(random_seed=11, mode='test')
+
+    if torch.cuda.is_available():
+        g_cuda = torch.Generator(device='cuda')
+    else:
+        g_cuda = torch.Generator()
+
+    g_cpu = torch.Generator(device='cpu')
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+    train_loader = DataLoader(train_data, batch_size=128, shuffle=True, generator=g_cpu)
+    valid_loader = DataLoader(valid_data, batch_size=128, shuffle=True, generator=g_cpu)
+    test_loader = DataLoader(test_data, batch_size=128, shuffle=True, generator=g_cpu)
+    X_train, Y_train = loader_to_numpy(train_loader)
+    X_valid, Y_valid = loader_to_numpy(valid_loader)
+    X_test, Y_test = loader_to_numpy(test_loader)
+
+    svm_model = SVC(kernel='linear', probability=True, decision_function_shape='ovr')  # 使用线性核函数，并启用概率估计
+    svm_model.fit(X_train, Y_train)
+
+    print("Training Set Evaluation:")
+    # evaluate_svm(X_train, Y_train, svm_model)
+    evaluate_three_svm(X_train, Y_train, svm_model)
+
+    print("Validation Set Evaluation:")
+    # evaluate_svm(X_valid, Y_valid, svm_model)
+    evaluate_three_svm(X_valid, Y_valid, svm_model)
+
+    print("Test Set Evaluation:")
+    # evaluate_svm(X_test, Y_test, svm_model)
+    evaluate_three_svm(X_test, Y_test, svm_model)
